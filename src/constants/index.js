ac41eaba91348078a0de551d487e99d909849712
dprasant <dprasant@purdue.edu>
2024-07-11 23:41:51 -0500

    changed hero constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,5 @@
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
-
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const HERO_CONTENT = `Graduating in December 2024 from Purdue University with a double major in Computer Science and Data Science, concentrating in Machine Learning, I have deep expertise in developing innovative and scalable solutions. My hands-on experience through research, software engineering, and machine learning internships have allowed me to explore roles in data analytics, machine learning, and full-stack development. I am now seeking a full-time position to leverage my diverse skills and drive impactful business outcomes.`;
 
 export const EXPERIENCES = [
   {
