703957f9991973261c0da59cd102fd405371aa35
dprasant <dprasant@purdue.edu>
2024-07-16 23:14:22 -0500

    added linkedin and github links to icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
-import  { FaTwitter } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
 
 
 const Navbar = () => {
+
+  const openResume = () => {
+    window.open('https://example.com/your-resume.pdf', '_blank');
+  };
+
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        
+        {/* Logo or other content */}
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaTwitter />
-        <FaInstagram />
+      
+      <a href="https://www.linkedin.com/in/diyaprasanth/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+      </a>
+      <a href="https://github.com/DiyaPrasanth20" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+      </a>
+
+      <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={openResume}
+        >
+          View Resume
+      </button>
+
     </div>
   </nav>
 }
@@ -24,6 +38,6 @@ export default Navbar
 
 /*
 TO DO:
-- find another logo or none at all (cause need to add stuff that will scroll to relevant pages)
-- add links to icons 
+- fix the view resume button 
+- 
 */
