e0b2ec73099eddd30741ce7dd7707c3706aa55dd
dprasant <dprasant@purdue.edu>
2024-07-16 22:43:03 -0500

    added more todos and icons

--- src/components/languages.jsx
+++ src/components/languages.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import javaLogo from '../assets/java_logo.png'; 
 import pythonLogo from '../assets/python_logo.png';
+import CLogo from '../assets/C_logo.png';
+import CPlusPlusLogo from '../assets/CPlusPlus_logo.png';
+import CSharpLogo from '../assets/CSharp_logo.png';
+import { FaGolang } from "react-icons/fa6";
+import { SiR } from "react-icons/si";
 
 const Languages = () => {
   return (
@@ -27,6 +32,44 @@ const Languages = () => {
 
                 />
         </div>
+
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img
+                    src={CLogo}
+                    alt="C Logo"
+                    className="h-16 w-auto"
+                    style={{ height: '70px', width: 'auto' }}
+
+                />
+        </div>
+
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img
+                    src={CPlusPlusLogo}
+                    alt="C++ Logo"
+                    className="h-16 w-auto"
+                    style={{ height: '70px', width: 'auto' }}
+
+                />
+        </div>
+
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img
+                    src={CSharpLogo}
+                    alt="C# Logo"
+                    className="h-16 w-auto"
+                    style={{ height: '70px', width: 'auto' }}
+
+                />
+        </div>
+
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                < FaGolang className="text-7xl" style={{ color: '#008AD7' }} />
+        </div>
+
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                < SiR className="text-7xl" style={{ color: '#00009B' }} />
+        </div>
                     
             
         </div>
@@ -42,7 +85,10 @@ TO DO:
 - when you hover over languages it should tell you what it is or 
 when you click on it it should tell you what it is. Some way to tell you what it is.
 
--   C/C++/C#, Go, R, Shell Scripting, JavaScript, HTML/CSS
+-  Shell Scripting, JavaScript, HTML/CSS
+
+
+- could probably change the c icons to the react icons 
 
 
 */
